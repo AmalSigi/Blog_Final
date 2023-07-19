5f43de782e17e58a8d33eb44ea798960122ade5a
Amal Sigi <amalsiji4764@gmail.com>
2023-07-19 08:48:15 +0530

    new

--- src/app/modules/user-modules/content/content.module.ts
+++ src/app/modules/user-modules/content/content.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContentRoutingModule } from './content-routing.module';
-import { ContentComponent } from './content.component';
-
+import { ContentComponent } from './root/content.component';
 
 @NgModule({
-  declarations: [
-    ContentComponent
-  ],
-  imports: [
-    CommonModule,
-    ContentRoutingModule
-  ]
+  declarations: [ContentComponent],
+  imports: [CommonModule, ContentRoutingModule],
 })
-export class ContentModule { }
+export class ContentModule {}
