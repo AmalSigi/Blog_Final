9119f85f150750d67515071e60b78a517adaccb5
Amal Sigi <amalsiji4764@gmail.com>
2023-07-20 15:17:31 +0530

    add category

--- src/app/modules/admin-modules/pages/admin-settings/pages/admin-category/admin-category.component.ts
+++ src/app/modules/admin-modules/pages/admin-settings/pages/admin-category/admin-category.component.ts
@@ -5,15 +5,4 @@ import { postData } from 'src/app/core/services/posts.services';
   selector: 'app-admin-category',
   templateUrl: './admin-category.component.html',
 })
-export class AdminCategoryComponent implements OnInit {
-  constructor(private readonly cat: postData) {}
-  public category: any;
-  ngOnInit(): void {
-    this.getCategory();
-  }
-
-  public getCategory() {
-    this.category = this.cat.Category;
-    console.log(this.category);
-  }
-}
+export class AdminCategoryComponent {}
